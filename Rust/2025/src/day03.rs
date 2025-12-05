cacbc21b6f89383e07428940e926619745bcacf7
kestrel-x86 <40702009+kestrel-x86@users.noreply.github.com>
2025-12-04 22:20:40 -0500

    Day 3 and 4

--- Rust/2025/src/day03.rs
+++ Rust/2025/src/day03.rs
@@ -2,25 +2,45 @@ use crate::Base;
 use std::fmt::Display;
 
 pub struct Day03 {
-    input: String,
+    input: Vec<Vec<u8>>,
 }
 
 impl Day03 {
     pub fn new() -> Day03 {
-        return Day03 { input: String::new() };
+        return Day03 { input: Vec::new() };
     }
 }
 
 impl Base for Day03 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        self.input = raw_input.lines().map(|l| l.bytes().collect()).collect();
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut total = 0usize;
+        for bank in &self.input {
+            let tens = bank[..(bank.len() - 1)].iter().max().unwrap();
+            let first_max = bank.iter().position(|x| x == tens).unwrap();
+            let ones = bank[(first_max + 1)..].iter().max().unwrap();
+            let tens = tens - 48;
+            let ones = ones - 48;
+            total += ((tens * 10) + ones) as usize;
+        }
+
+        return Box::new(total);
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut total = 0;
+        for bank in &self.input {
+            let mut bank: &[u8] = bank;
+            for place in (0..12).rev() {
+                let num = bank[..(bank.len() - place)].iter().max().unwrap();
+                let num_i = bank.iter().position(|x| x == num).unwrap();
+                bank = &bank[(num_i + 1)..];
+                total += (*num - 48) as usize * 10usize.pow(place as u32);
+            }
+        }
+        return Box::new(total); // 172664333119298
     }
 }
--- Rust/2025/src/day04.rs
+++ Rust/2025/src/day04.rs
@@ -1,26 +1,106 @@
+use shared::bitset::Bitset2D;
+
 use crate::Base;
-use std::fmt::Display;
+use std::{collections::HashSet, fmt::Display};
+
+const PAPER: u8 = b'@';
+
+const NEIGHBORS: [(isize, isize); 8] = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+];
 
 pub struct Day04 {
-    input: String,
+    input: Bitset2D,
 }
 
 impl Day04 {
     pub fn new() -> Day04 {
-        return Day04 { input: String::new() };
+        return Day04 {
+            input: Bitset2D::new(0, 0),
+        };
     }
 }
 
 impl Base for Day04 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        let sz = (raw_input.len() as f64).sqrt() as usize;
+        let mut map = Bitset2D::new(sz, sz);
+        for (y, line) in raw_input.lines().enumerate() {
+            for (x, b) in line.as_bytes().iter().enumerate() {
+                if *b == PAPER {
+                    map.set(x, y);
+                }
+            }
+        }
+        self.input = map;
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let sz = self.input.rows() as isize;
+
+        let mut count = 0;
+        for (x, y) in self.input.iter_set() {
+            let mut paper_count = 0;
+            for neighbor in NEIGHBORS {
+                let nx = x as isize + neighbor.0;
+                let ny = y as isize + neighbor.1;
+                if nx >= 0
+                    && ny >= 0
+                    && nx < sz
+                    && ny < sz
+                    && self.input.is_set(nx as usize, ny as usize)
+                {
+                    paper_count += 1;
+                }
+            }
+            if paper_count < 4 {
+                count += 1;
+            }
+        }
+
+        return Box::new(count);
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let sz = self.input.rows() as isize;
+        let mut removable = HashSet::new();
+        let mut total = 0;
+
+        loop {
+            for (x, y) in self.input.iter_set() {
+                let mut paper_count = 0;
+                for neighbor in NEIGHBORS {
+                    let nx = x as isize + neighbor.0;
+                    let ny = y as isize + neighbor.1;
+                    if nx >= 0
+                        && ny >= 0
+                        && nx < sz
+                        && ny < sz
+                        && self.input.is_set(nx as usize, ny as usize)
+                    {
+                        paper_count += 1;
+                    }
+                }
+                if paper_count < 4 && removable.insert((x, y)) {
+                    total += 1;
+                }
+            }
+            if removable.len() == 0 {
+                break;
+            }
+
+            for (x, y) in removable.drain() {
+                self.input.unset(x, y);
+            }
+        }
+
+        return Box::new(total);
     }
 }
