48a8118e7519511dbfb0afcfcbe71c2380cb9dcd
kestrel-x86 <40702009+kestrel-x86@users.noreply.github.com>
2025-12-09 23:59:45 -0500

    Day 9

--- Rust/2025/src/day09.rs
+++ Rust/2025/src/day09.rs
@@ -1,26 +1,115 @@
 use crate::Base;
+use shared::v2i::Vector2Int as V2;
 use std::fmt::Display;
 
 pub struct Day09 {
-    input: String,
+    input: Vec<V2>,
 }
 
 impl Day09 {
     pub fn new() -> Day09 {
-        return Day09 { input: String::new() };
+        return Day09 { input: Vec::new() };
     }
 }
 
 impl Base for Day09 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        self.input = raw_input
+            .lines()
+            .map(|line| {
+                let (a, b) = line.split_once(',').unwrap();
+                V2::new(a.parse().unwrap(), b.parse().unwrap())
+            })
+            .collect();
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut a = 0;
+        for i in 0..(self.input.len() - 1) {
+            for j in (i + 1)..self.input.len() {
+                a = a.max(
+                    (self.input[i].x - self.input[j].x + 1).abs()
+                        * (self.input[i].y - self.input[j].y + 1).abs(),
+                );
+            }
+        }
+
+        return Box::new(a);
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut a = 0;
+        for i in 0..(self.input.len() - 1) {
+            for j in (i + 1)..self.input.len() {
+                let mut y_range = [self.input[i].y, self.input[j].y];
+                y_range.sort();
+                let mut x_range = [self.input[i].x, self.input[j].x];
+                x_range.sort();
+
+                let mut fail = false;
+                for k in 0..(self.input.len() - 1) {
+                    if self.input[k].x == self.input[k + 1].x {
+                        fail = intersects_v(
+                            self.input[k].x,
+                            self.input[k].y,
+                            self.input[k + 1].y,
+                            &x_range,
+                            &y_range,
+                        );
+                    } else {
+                        fail = intersects_h(
+                            self.input[k].y,
+                            self.input[k].x,
+                            self.input[k + 1].x,
+                            &x_range,
+                            &y_range,
+                        );
+                    }
+                    if fail {
+                        break;
+                    }
+                }
+                if !fail {
+                    a = a.max(
+                        ((self.input[i].x - self.input[j].x).abs() + 1)
+                            * ((self.input[i].y - self.input[j].y).abs() + 1),
+                    );
+                }
+            }
+        }
+        return Box::new(a);
     }
+}
+
+fn intersects_h(
+    line_y: isize,
+    line_x1: isize,
+    line_x2: isize,
+    rect_x: &[isize; 2],
+    rect_y: &[isize; 2],
+) -> bool {
+    let line_x_min = line_x1.min(line_x2);
+    let line_x_max = line_x1.max(line_x2);
+    if !(rect_y[0] < line_y && line_y < rect_y[1]) {
+        return false;
+    }
+
+    return line_x_max > rect_x[0] && line_x_min < rect_x[1];
+}
+
+fn intersects_v(
+    line_x: isize,
+    line_y1: isize,
+    line_y2: isize,
+    rect_x: &[isize; 2],
+    rect_y: &[isize; 2],
+) -> bool {
+    let line_y_min = line_y1.min(line_y2);
+    let line_y_max = line_y1.max(line_y2);
+
+    if !(rect_x[0] < line_x && line_x < rect_x[1]) {
+        return false;
+    }
+
+    return line_y_max > rect_y[0] && line_y_min < rect_y[1];
 }
